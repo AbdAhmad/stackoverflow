85f22ad917af0eb5c0ef1d7db712360c7f427af3
AbdAhmad <Olamic@maggnet.io>
2024-06-01 20:57:16 +0530

    added question view page

---
+++ src/app/(main)/question/[id]/[slug]/page.jsx
@@ -0,0 +1,21 @@
+import Editor from "@/components/JoditEditor/JoditEditor";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Embedded Tomcat 10 serving static resources from fat jar</h1>
+      <p>
+        I'm writing some browser code with a lot of large ArrayBuffers. I
+        noticed the memory consumption is unexpectedly large and I'm not sure
+        why that is. I've never worked with ArrayBuffers before and I don't
+        understand it too well. But my test code is short so I'll include it
+        here:
+      </p>
+      <h2>Your Answer</h2>
+      <Editor />
+    </div>
+  );
+};
+
+export default Page;
--- src/app/(main)/questions/page.jsx
+++ src/app/(main)/questions/page.jsx
@@ -1,10 +1,23 @@
 import Questions from "@/components/Questions/Questions";
+import Link from "next/link";
 import React from "react";
 
 const Page = () => {
   return (
-    <div>
-      <Questions />
+    <div className="grid grid-cols-12 gap-4">
+      <div className="col-span-1">
+        {[{ text: "Questions" }].map((link, index) => {
+          const { text } = link;
+          return (
+            <Link key={index} href={text}>
+              {text}
+            </Link>
+          );
+        })}
+      </div>
+      <div className="col-span-11">
+        <Questions />
+      </div>
     </div>
   );
 };
