a8d385448b43a199244ef7b54f634b464ee9b89f
AbdAhmad <Olamic@maggnet.io>
2024-07-04 00:39:40 +0530

    added ask question  page

---
+++ src/app/(main)/ask-question/page.jsx
@@ -0,0 +1,13 @@
+// import Editor from "@/components/JoditEditor/JoditEditor";
+import AskQuestion from "@/components/AskQuestion/AskQuestion";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div>
+      <AskQuestion />
+    </div>
+  );
+};
+
+export default Page;
--- src/app/(main)/question/[id]/[slug]/page.jsx
+++ src/app/(main)/question/[id]/[slug]/page.jsx
@@ -6,11 +6,11 @@ const Page = () => {
     <div>
       <h1>Embedded Tomcat 10 serving static resources from fat jar</h1>
       <p>
-        I'm writing some browser code with a lot of large ArrayBuffers. I
-        noticed the memory consumption is unexpectedly large and I'm not sure
-        why that is. I've never worked with ArrayBuffers before and I don't
-        understand it too well. But my test code is short so I'll include it
-        here:
+        I&apos;m writing some browser code with a lot of large ArrayBuffers. I
+        noticed the memory consumption is unexpectedly large and I&apos;m not
+        sure why that is. I&apos;ve never worked with ArrayBuffers before and I
+        don&apos;t understand it too well. But my test code is short so
+        I&apos;ll include it here:
       </p>
       <h2>Your Answer</h2>
       <Editor />
